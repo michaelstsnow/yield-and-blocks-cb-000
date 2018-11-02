1ff01c3d657749cd8ea9c3f3b5421b9a8ae83362
michaelstsnow <michael.st.snow@gmail.com>
2018-11-02 23:14:39 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,17 @@
-def hello_t
-
+def hello_t(names)
+  if block_given?
+    i=0;
+    while 1<names.size
+      yield(names)
+      i+=1;
+    end
+    names
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
 # call your method here!
 
+names=["Mike","John","Chris"];
+hello_t(names)

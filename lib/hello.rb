c9210c09732b7222c761171e3cac8405e78a8dcf
michaelstsnow <michael.st.snow@gmail.com>
2018-11-02 23:01:57 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,13 @@
 def hello_t(names)
+  if names.size==0
+    puts
+  else
   names.each do |name|
     big_name=name.upcase;
     puts "#{big_name}"
   end
   names
+end
 end
 
 # call your method here!
